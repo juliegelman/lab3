16830eb2f16649df0062310e8ed6a4619f8fe3d7
Julie <jgelman@hawk.iit.edu>
2018-10-03 22:13:32 -0500

    type smh

--- js/site.js
+++ js/site.js
@@ -1,4 +1,4 @@
-//jQuery 3.x-style ready event and locaaly scoped $
+//jQuery 3.x-style ready event and locally scoped $
 jQuery(function($) {
   $('html').removeClass('nojs');
   $('html').addClass('hasjs');
